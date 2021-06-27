e6e5490415aca3496c56d75fcf021fbc5b101653
sshwy <jy.cat@qq.com>
2021-06-27 15:11:55 +0800

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,6 @@ process.env.VUE_APP_BUILD_TIME = (new Date()).toString()
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
+  publicPath: '/static/beibishi2021/', // 部署到博客上
   // 选项...
 }
